18856227752fe3b7a53314c20820dcbfa7745de7
jessie <jiaqi.zheng@aalto.fi>
2017-12-30 02:13:17 +0200

    cursor speed for valley done; delete some redundant code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 13,
         center: {lat: 63.203, lng: -150.4},  // Denali.
         mapTypeId: 'terrain'
     });
@@ -55,62 +55,47 @@ function displayLocationElevation(location, elevator, infowindow) {
 // config
 
 const kAssets = {
-  mac_retina: {
-    normal: {
-      src: 'assets/mac_retina.png',
-      height: '22px',
-      width: '15px'
+    mac_retina: {
+        normal: {
+            src: 'assets/mac_retina.png',
+            height: '22px',
+            width: '15px'
+        },
     },
-    pointer: {
-      src: 'assets/mac_pointer_retina.png',
-      width: '18.5px',
-      height: '19.5px'
-    }
-  },
-  mac: {
-    normal: {
-      src: 'assets/mac.png',
-      width: '15px',
-      height: '22px'
+    mac: {
+        normal: {
+            src: 'assets/mac.png',
+            width: '15px',
+            height: '22px'
+        },
     },
-    pointer: {
-      src: 'assets/mac_pointer.png',
-      width: '18px',
-      height: '19px'
+    other: {
+        normal: {
+            src: 'assets/other.png',
+            width: '17px',
+            height: '23px'
+        },
     }
-  },
-  other: {
-    normal: {
-      src: 'assets/other.png',
-      width: '17px',
-      height: '23px'
-    },
-    pointer: {
-      src: 'assets/other_pointer.png',
-      width: '22px',
-      height: '26px'
-    }
-  }
 };
 
 function getCursor() {
-  let navigator = window.navigator.platform.indexOf('Mac') > -1 ? 'mac' : 'win';
+    let navigator = window.navigator.platform.indexOf('Mac') > -1 ? 'mac' : 'win';
 
-  if (window.devicePixelRatio > 1) {
-    navigator += '_retina';
-  }
+    if (window.devicePixelRatio > 1) {
+        navigator += '_retina';
+    }
 
-  if (Object.keys(kAssets).includes(navigator)) {
-    return kAssets[navigator];
-  }
+    if (Object.keys(kAssets).includes(navigator)) {
+        return kAssets[navigator];
+    }
 
-  return kAssets.other;
+    return kAssets.other;
 }
 
 function setCursor(cursorConfig) {
-  C.el.src = cursorConfig.src;
-  C.el.style.width = cursorConfig.width;
-  C.el.style.height = cursorConfig.height;
+    C.el.src = cursorConfig.src;
+    C.el.style.width = cursorConfig.width;
+    C.el.style.height = cursorConfig.height;
 }
 
 // global
@@ -131,243 +116,214 @@ C.elToTrackKeys = Object.keys(C.elToTrack);
 C.isMouseVisible = false;
 
 function ControlUserCursor(config, createElements) {
-  const elConfig = config.el;
+    const elConfig = config.el;
 
-  C.expoWeight = config.expoWeight || 2;
-
-  if (createElements) {
-    C.containerEl.innerHTML = '';
-  }
-
-  C.elToTrack = elConfig;
-
-  C.elToTrackKeys = Object.keys(C.elToTrack);
-  C.elToTrackKeys.map(key => {
-    if (elConfig[key].el === undefined) {
-      C.elToTrack[key].el = document.createElement('div');
-      C.elToTrack[key].el.className = elConfig[key].className.join(' ');
-      C.elToTrack[key].el.innerHTML = elConfig[key].innerHTML;
-    }
-    C.elToTrack[key]._hover = false;
+    C.expoWeight = config.expoWeight || 2;
 
     if (createElements) {
-      C.containerEl.appendChild(C.elToTrack[key].el);
+        C.containerEl.innerHTML = '';
     }
-  });
 
-  onUpdateElementSizes();
+    C.elToTrack = elConfig;
+
+    C.elToTrackKeys = Object.keys(C.elToTrack);
+    C.elToTrackKeys.map(key => {
+        if (elConfig[key].el === undefined) {
+            C.elToTrack[key].el = document.createElement('div');
+            C.elToTrack[key].el.className = elConfig[key].className.join(' ');
+            C.elToTrack[key].el.innerHTML = elConfig[key].innerHTML;
+        }
+        C.elToTrack[key]._hover = false;
+
+        if (createElements) {
+            C.containerEl.appendChild(C.elToTrack[key].el);
+        }
+    });
+
+    onUpdateElementSizes();
 }
 
 // UTILS
 function getPolarity(num) {
-  return num >= 0 ? 1 : -1;
+    return num >= 0 ? 1 : -1;
 }
 
 function getDiff(obj1, obj2) {
-  const diffX = obj1.x - obj2.x;
-  const diffY = obj1.y - obj2.y;
-  return Math.sqrt(diffX * diffX + diffY * diffY);
+    const diffX = obj1.x - obj2.x;
+    const diffY = obj1.y - obj2.y;
+    return Math.sqrt(diffX * diffX + diffY * diffY);
 }
 
 // does the math, using the elToTrack config object
 function calculateNewCursor(newCursor) {
-  const calculatedCursor = {
-    x: newCursor.x,
-    y: newCursor.y
-  };
+    const calculatedCursor = {
+        x: newCursor.x,
+        y: newCursor.y
+    };
 
-  C.elToTrackKeys.map(key => {
-    const objSize = C.elToTrack[key]._size;
-    const objCenter = C.elToTrack[key]._center;
-    const objBehavior = C.elToTrack[key].behavior;
+    C.elToTrackKeys.map(key => {
+        const objSize = C.elToTrack[key]._size;
+        const objCenter = C.elToTrack[key]._center;
+        const objBehavior = C.elToTrack[key].behavior;
 
-    const diff = getDiff(objCenter, newCursor);
+        const diff = getDiff(objCenter, newCursor);
 
-    const importance = Math.pow(1 - Math.min(1, Math.max(diff / C.wZ, 0)), C.expoWeight);
+        const importance = Math.pow(1 - Math.min(1, Math.max(diff / C.wZ, 0)), C.expoWeight);
 
-    if (importance < 0.001) {
-      return newCursor;
-    }
+        if (importance < 0.001) {
+            return newCursor;
+        }
 
-    let xyDiff;
+        let xyDiff;
 
-    if (objBehavior === 'MOUNTAIN') {
-      const angle = Math.atan2(objCenter.y - newCursor.y, objCenter.x - newCursor.x);
-      const offset = objSize * importance / 1.4142;
+        xyDiff = {
+            x: objCenter.x - (objCenter.x * importance + newCursor.x * (1 - importance)),
+            y: objCenter.y - (objCenter.y * importance + newCursor.y * (1 - importance))
+        }
+        if(objBehavior === 'MOUNTAIN'){
+            calculatedCursor.x = calculatedCursor.x - newCursor.x + (objCenter.x - xyDiff.x);
+            calculatedCursor.y = calculatedCursor.y - newCursor.y + (objCenter.y - xyDiff.y);
+        }else if (objBehavior === 'VALLEY'){
+            calculatedCursor.x = calculatedCursor.x + newCursor.x - (objCenter.x - xyDiff.x);
+            calculatedCursor.y = calculatedCursor.y + newCursor.y - (objCenter.y - xyDiff.y);
+        }
+    });
 
-      xyDiff = {
-        x: Math.cos(angle) * (diff + offset),
-        y: Math.sin(angle) * (diff + offset)
-      };
-    } else if (objBehavior === 'VALLEY') {
-      xyDiff = {
-        x: objCenter.x - (objCenter.x * importance + newCursor.x * (1 - importance)),
-        y: objCenter.y - (objCenter.y * importance + newCursor.y * (1 - importance))
-      };
-    }
-
-    calculatedCursor.x = calculatedCursor.x - newCursor.x + (objCenter.x - xyDiff.x);
-    calculatedCursor.y = calculatedCursor.y - newCursor.y + (objCenter.y - xyDiff.y);
-  });
-
-  return {
-    x: Math.round(calculatedCursor.x),
-    y: Math.round(calculatedCursor.y)
-  };
+    return {
+        x: Math.round(calculatedCursor.x),
+        y: Math.round(calculatedCursor.y)
+    };
 }
 
 // iterate over the elements to see if we need to hover anyone
 function calculateHover(newCursor) {
-  C.elToTrackKeys.map(key => {
-    const trackedObj = C.elToTrack[key];
+    C.elToTrackKeys.map(key => {
+        const trackedObj = C.elToTrack[key];
 
-    if (trackedObj.behavior !== 'VALLEY') {
-      return;
-    }
+        if (trackedObj.behavior !== 'VALLEY') {
+            return;
+        }
 
-    const calculatedDiff = getDiff(trackedObj._center, newCursor);
+        const calculatedDiff = getDiff(trackedObj._center, newCursor);
 
-    const isHovering = calculatedDiff < trackedObj._size / 2;
+        const isHovering = calculatedDiff < trackedObj._size / 2;
 
-    if (isHovering === true && trackedObj._hover === false) {
-      setCursor(C.cursorConfig.pointer);
+        setCursor(C.cursorConfig.normal);
 
-      trackedObj._hover = true;
-      trackedObj.el.classList.add('-hover');
-    } else if (isHovering === false && trackedObj._hover === true) {
-      setCursor(C.cursorConfig.normal);
-
-      trackedObj._hover = false;
-      trackedObj.el.classList.remove('-hover');
-    }
-  });
+        trackedObj._hover = false;
+        trackedObj.el.classList.remove('-hover');
+    });
 }
 
 // remove the fake cursor when the user moves the real out of the window
 function onMouseOut() {
-  window.requestAnimationFrame(() => {
-    C.el.style.opacity = 0;
-    C.isMouseVisible = false;
-  });
+    window.requestAnimationFrame(() => {
+        C.el.style.opacity = 0;
+        C.isMouseVisible = false;
+    });
 }
 
 // main function that calculates the fake cursor position
 function onMouseMove(evt) {
-  window.requestAnimationFrame(() => {
-    if (C.isMouseVisible === false) {
-      C.el.style.opacity = 1;
-      C.isMouseVisible = true;
-    }
+    window.requestAnimationFrame(() => {
+        if (C.isMouseVisible === false) {
+            C.el.style.opacity = 1;
+            C.isMouseVisible = true;
+        }
 
-    const calculatedCursor = calculateNewCursor({
-      x: evt.clientX,
-      y: evt.clientY
+        const calculatedCursor = calculateNewCursor({
+            x: evt.clientX,
+            y: evt.clientY
+        });
+
+        calculateHover(calculatedCursor);
+
+        C.el.style.transform =
+            'translatex(' + calculatedCursor.x + 'px) translatey(' + calculatedCursor.y + 'px)';
     });
-
-    calculateHover(calculatedCursor);
-
-    C.el.style.transform =
-      'translatex(' + calculatedCursor.x + 'px) translatey(' + calculatedCursor.y + 'px)';
-  });
 }
 
-function onClick() {
-  const clickedEl = document.querySelector('.-hover');
-
-  if (!clickedEl) return;
-
-  clickedEl.innerHTML = clickedEl.innerHTML === ':D' ? 'VALLEY!' : ':D';
-}
 
 function onUpdateElementSizes() {
-  window.requestAnimationFrame(() => {
-    // update the window size values
-    C.wX = window.innerWidth;
-    C.wY = window.innerHeight;
-    C.wZ = Math.min(C.wX, C.wY) / 2;
+    window.requestAnimationFrame(() => {
+        // update the window size values
+        C.wX = window.innerWidth;
+        C.wY = window.innerHeight;
+        C.wZ = Math.min(C.wX, C.wY) / 2;
 
-    // iterate over all the elements that we are tracking, and update the clientRect value
-    C.elToTrackKeys.map(key => {
-      const clientRect = C.elToTrack[key].el.getBoundingClientRect();
+        // iterate over all the elements that we are tracking, and update the clientRect value
+        C.elToTrackKeys.map(key => {
+            const clientRect = C.elToTrack[key].el.getBoundingClientRect();
 
-      // update element internal vals
-      C.elToTrack[key]._clientRect = clientRect;
-      C.elToTrack[key]._center = {
-        x: clientRect.left + clientRect.width / 2,
-        y: clientRect.top + clientRect.height / 2
-      };
-      C.elToTrack[key]._size = Math.max(clientRect.width, clientRect.height);
+            // update element internal vals
+            C.elToTrack[key]._clientRect = clientRect;
+            C.elToTrack[key]._center = {
+                x: clientRect.left + clientRect.width / 2,
+                y: clientRect.top + clientRect.height / 2
+            };
+            C.elToTrack[key]._size = Math.max(clientRect.width, clientRect.height);
+        });
     });
-  });
 }
 
-window.addEventListener('click', onClick);
 window.addEventListener('resize', onUpdateElementSizes);
 window.addEventListener('mouseout', onMouseOut);
 window.addEventListener('contextmenu', event => event.preventDefault());
 
-Array.from(document.querySelectorAll('.-prevent-custom-cursor')).forEach(link => {
-  link.addEventListener('mouseover', evt => {
-    setCursor(C.cursorConfig.pointer);
-  });
-  link.addEventListener('mouseleave', evt => {
-    setCursor(C.cursorConfig.normal);
-  });
-});
 
 // initialize sizes
 
 const kConfig = {
-  basic: {
-    el: {
-      buttonToPrevent: {
-        behavior: 'MOUNTAIN',
-        innerHTML: 'Mountain',
-        className: ['clickme', '-mountain']
-      }
+    basic: {
+        el: {
+            buttonToPrevent: {
+                behavior: 'MOUNTAIN',
+                innerHTML: 'Mountain',
+                className: ['clickme', '-mountain']
+            }
+        },
+        expoWeight: 1
     },
-    expoWeight: 1
-  },
-//  doble: {
-//    el: {
-//      buttonToPrevent: {
-//        behavior: 'MOUNTAIN',
-//        innerHTML: 'Nope :P',
-//        className: ['clickme', '-mountain', '-alt1']
-//      },
-//      buttonToPrevent2: {
-//        behavior: 'MOUNTAIN',
-//        innerHTML: 'Mountain',
-//        className: ['clickme', '-mountain', '-alt2']
-//      }
-//    },
-//    expoWeight: 2
-//  },
-  valley: {
-    el: {
-      buttonToValley: {
-        behavior: 'VALLEY',
-        innerHTML: 'Valley',
-        className: ['clickme']
-      }
+    //  doble: {
+    //    el: {
+    //      buttonToPrevent: {
+    //        behavior: 'MOUNTAIN',
+    //        innerHTML: 'Nope :P',
+    //        className: ['clickme', '-mountain', '-alt1']
+    //      },
+    //      buttonToPrevent2: {
+    //        behavior: 'MOUNTAIN',
+    //        innerHTML: 'Mountain',
+    //        className: ['clickme', '-mountain', '-alt2']
+    //      }
+    //    },
+    //    expoWeight: 2
+    //  },
+    valley: {
+        el: {
+            buttonToValley: {
+                behavior: 'VALLEY',
+                innerHTML: 'Valley',
+                className: ['clickme']
+            }
+        },
+        expoWeight: 2
     },
-    expoWeight: 2
-  },
-//  combined: {
-//    el: {
-//      buttonToPrevent: {
-//        behavior: 'MOUNTAIN',
-//        innerHTML: 'Mountain',
-//        className: ['clickme', '-mountain', '-alt1']
-//      },
-//      buttonToValley: {
-//        behavior: 'VALLEY',
-//        innerHTML: 'Valley!',
-//        className: ['clickme', '-alt2']
-//      }
-//    },
-//    expoWeight: 2
-//  }
+    //  combined: {
+    //    el: {
+    //      buttonToPrevent: {
+    //        behavior: 'MOUNTAIN',
+    //        innerHTML: 'Mountain',
+    //        className: ['clickme', '-mountain', '-alt1']
+    //      },
+    //      buttonToValley: {
+    //        behavior: 'VALLEY',
+    //        innerHTML: 'Valley!',
+    //        className: ['clickme', '-alt2']
+    //      }
+    //    },
+    //    expoWeight: 2
+    //  }
 };
 
 ControlUserCursor(kConfig.basic, true);
